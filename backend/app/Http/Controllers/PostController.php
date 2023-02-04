da2b81d22626bf659e70d25ab315038dd6f1c129
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2023-02-04 15:30:36 +0600

    post crud done

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::latest()->get();
+        $posts = Post::select(['id','name','description'])->latest()->get();
 
         return response()->json([
             'data' => $posts
@@ -27,44 +27,66 @@ class PostController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+        ]);
+
+        $post = new Post();
+        $post->name = $request->name;
+        $post->description = $request->description;
+        $post->save();
+
+        return response()->json(['message' => 'Post created successfully.'],201);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show(Post $post)
     {
-        //
+        return response()->json([
+            'data' => $post
+        ],200);
     }
-    
+
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+        ]);
+
+        $post->name = $request->name;
+        $post->description = $request->description;
+        $post->save();
+
+        return response()->json(['message' => 'Post updated successfully.'],201);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return response()->json(['message' => 'Post deleted successfully.'],200);
     }
 }
